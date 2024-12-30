a58816534b92ac5545854971ff0886e3635a8ecc
Diego Rolando Layme Valeriano <vandiego_pandax@hotmail.com>
2024-12-29 22:09:30 -0500

    requirement for user diferent admin

--- app/Models/ProjectRequirement.php
+++ app/Models/ProjectRequirement.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class ProjectRequirement extends Model
 {
     use SoftDeletes;
+
+    public function assignations(){
+        return $this->hasMany(ProjectRequirementAssigned::class,'project_requeriment_id','id');
+    }
 }
